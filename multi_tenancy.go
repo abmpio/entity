210ef3cb2f4c3b3d44251c12c64d105e447489de
jeseeqing <jeseeqing@gmail.com>
2024-10-16 11:12:31 +0800

    rename EntityWithTenant name to MultiTeancy name

---
+++ multi_tenancy.go
@@ -0,0 +1,53 @@
+package entity
+
+type IMultiTenancy interface {
+	SetTenantId(tenantId string)
+	GetTenantId() string
+
+	MustHaveTenantId() bool
+}
+
+// MultiTenancy entity
+type MultiTenancy struct {
+	TenantId string `json:"tenantId" bson:"tenantId"`
+}
+
+type MustHaveMultiTenancy struct {
+	MultiTenancy `bson:",inline"`
+}
+
+type MayHaveMultiTenancy struct {
+	MultiTenancy `bson:",inline"`
+}
+
+var _ IMultiTenancy = (*MultiTenancy)(nil)
+var _ IMultiTenancy = (*MustHaveMultiTenancy)(nil)
+var _ IMultiTenancy = (*MayHaveMultiTenancy)(nil)
+
+// #region IMultiTenancy Members
+
+func (t *MultiTenancy) SetTenantId(tenantId string) {
+	t.TenantId = tenantId
+}
+
+func (t *MultiTenancy) GetTenantId() string {
+	return t.TenantId
+}
+
+func (t *MultiTenancy) MustHaveTenantId() bool {
+	return false
+}
+
+// #endregion
+
+func (t *MustHaveMultiTenancy) MustHaveTenantId() bool {
+	return true
+}
+
+func CheckEntityIsIMultiTenancy(entityValue interface{}) IMultiTenancy {
+	v, ok := entityValue.(IMultiTenancy)
+	if !ok {
+		return nil
+	}
+	return v
+}
