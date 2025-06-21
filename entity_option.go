afcf52c851017bcc3de98eca74e1e318b971dda3
jeseeqing <jeseeqing@gmail.com>
2025-06-21 23:55:53 +0800

    feat: remove BsonBuilderOption func type

--- entity_option.go
+++ entity_option.go
@@ -1,10 +1,7 @@
 package entity
 
 import (
-	"github.com/abmpio/abmp/pkg/utils/lang"
-	"github.com/abmpio/libx/lang/tuple"
 	"github.com/abmpio/mongodbr"
-	mongodbBuilder "github.com/abmpio/mongodbr/builder"
 )
 
 func EntityWithCreatorUserId(userId string) mongodbr.EntityOption {
@@ -23,30 +20,5 @@ func ApplyEntityOption(e mongodbr.IEntity, opts ...mongodbr.EntityOption) {
 	}
 	for _, eachOpt := range opts {
 		eachOpt(e)
-	}
-}
-
-type BsonBuilderOption func(*mongodbBuilder.BsonBuilder)
-
-// new mongodbBuilder.BsonBuilder with option
-func NewBsonBuilderWithOption(opts ...BsonBuilderOption) *mongodbBuilder.BsonBuilder {
-	bsonBuilder := &mongodbBuilder.BsonBuilder{}
-	for _, eachOpt := range opts {
-		eachOpt(bsonBuilder)
-	}
-	return bsonBuilder
-}
-
-// set lastModificationTime field value to now
-func BsonBuilderOptionWithLastModificationTime() BsonBuilderOption {
-	return func(bb *mongodbBuilder.BsonBuilder) {
-		bb.AppendSetField(tuple.New2[string, interface{}]("lastModificationTime", lang.NowToPtr()))
-	}
-}
-
-// set lastModifierId field value to now
-func BsonBuilderOptionWithLastModifierId(userId string) BsonBuilderOption {
-	return func(bb *mongodbBuilder.BsonBuilder) {
-		bb.AppendSetField(tuple.New2[string, interface{}]("lastModifierId", userId))
 	}
 }
